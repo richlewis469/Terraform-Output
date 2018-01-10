7fdc57e1c3b882b7ebae55b535d1ed77a9ddeea5
Rich Lewis <rich.lewis@oracle.com>
2018-01-10 17:52:16 -0600

    Updated prints and docs

--- vagrant-shell/terraform-output.sh
+++ vagrant-shell/terraform-output.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Executing /vagrant-share/vagrant-terraform/Terraform"
-cd /vagrant-share/vagrant-terraform
 
+echo "  Remove any trailing \r dos characters"
+cd /vagrant-share
+find . -type f -exec dos2unix -ascii {} \;
+
+echo "  Setup the environment variables"
+cd /vagrant-share/vagrant-terraform
 source /vagrant-share/vagrant-terraform/env-vars.sh
 printenv | grep -i TF_VAR | sed "s/\=.*$//"
 
+echo "  Run terraform init"
 terraform init
+echo "  Run terraform plan"
 terraform plan
+echo "  Run terraform apply"
 terraform apply
 
 echo "Terminating /vagrant-share/vagrant-terraform/Terraform"
