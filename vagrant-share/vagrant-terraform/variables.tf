9aecddf06f607658b013d414e4d27687a7cd0a2b
Rich Lewis <rich.lewis@oracle.com>
2018-01-11 10:32:42 -0600

    Removed surplus testing variables.

--- vagrant-share/vagrant-terraform/variables.tf
+++ vagrant-share/vagrant-terraform/variables.tf
@@ -5,9 +5,5 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
-
-#variable "private_key" {}
-#variable "ssh_public_key" {}
 variable "compartment_ocid" {}
-
 variable "region" {}
