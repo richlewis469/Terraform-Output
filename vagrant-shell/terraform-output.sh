42edaf22c743a73b1a5085d92568fbc7087dc70d
Rich Lewis <rich.lewis@oracle.com>
2018-01-12 11:28:25 -0600

    Update to ignore .gitkeep

--- vagrant-shell/terraform-output.sh
+++ vagrant-shell/terraform-output.sh
@@ -4,7 +4,7 @@ echo "Executing /vagrant-share/vagrant-terraform/Terraform"
 
 echo "--Remove any trailing \r dos characters--"
 cd /vagrant-share
-for i in `find . -type f -print | file -f - | grep -v binary | grep CRLF | sed -e "s/\:.*$//"`; do
+for i in `find . -type f -print | file -f - | grep -v binary | grep -v gitkeep | grep CRLF | sed -e "s/\:.*$//"`; do
   dos2unix ${i}
 done
 
@@ -16,7 +16,7 @@ printenv | \
 grep -i TF_VAR | \
 sed '/ocid=/s/\.\.\(.\{10\}\).*/\.\.\1\**************************************************/' | \
 sed '/fingerprint=/s/=\(.\{15\}\).*/=\1**:**:**:**:**:**:**:**:**:**:**/' | \
-sort 
+sort
 
 echo "--Run terraform init--"
 terraform init
